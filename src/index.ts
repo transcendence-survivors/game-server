49ef225b494e0ae642b59bb46512ee8301f748b1
Thyanoui Lutz <tlutz@student.42lyon.fr>
2026-06-23 00:19:25 +0200

    TRY to fix CORS policy error between client and server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { BunWebSockets } from '@colyseus/bun-websockets';
 import { env } from 'bun';
-import { matchMaker, Room, Server } from 'colyseus';
+import { matchMaker, Server } from 'colyseus';
 import { GameRoom } from './GameRoom';
 
 matchMaker.controller.getCorsHeaders = () => ({
 	'Access-Control-Allow-Origin': 'http://localhost:5173',
 	'Access-Control-Allow-Credentials': 'true',
-	'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-	'Access-Control-Allow-Headers':
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-	Vary: 'Origin',
 });
 
 const gameServer = new Server({ transport: new BunWebSockets() });
@@ -17,5 +13,4 @@ const gameServer = new Server({ transport: new BunWebSockets() });
 gameServer.define('game', GameRoom);
 
 console.log('Listening on port 4000');
-// await gameServer.listen(env.PORT);
-const room = new GameRoom();
+await gameServer.listen(env.PORT);
