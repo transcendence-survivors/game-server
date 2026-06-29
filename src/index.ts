df457341917a9575de329c8a620620763f9087d3
Thyanoui Lutz <tlutz@student.42lyon.fr>
2026-06-29 21:15:42 +0200

    ADD open to all ips

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,18 @@ import { matchMaker, Server } from 'colyseus';
 import { GameRoom } from './GameRoom';
 import { BunWebSockets } from '@colyseus/bun-websockets';
 
-matchMaker.controller.getCorsHeaders = () => ({
-	'Access-Control-Allow-Origin': 'http://localhost:5173',
+matchMaker.controller.getCorsHeaders = (reqHeaders) => ({
+	'Access-Control-Allow-Origin': reqHeaders.get('origin') || '*',
 	'Access-Control-Allow-Credentials': 'true',
 	'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
 	'Access-Control-Allow-Headers':
 		'Origin, X-Requested-With, Content-Type, Accept, Authorization',
 });
 
+const transport = new BunWebSockets();
+
 const gameServer = new Server({ transport: new BunWebSockets() });
 
 gameServer.define('game', GameRoom);
 
-console.log('Listening on port 4000');
-await gameServer.listen(env.PORT);
+await gameServer.listen(Number(env.PORT ?? 4000), '0.0.0.0');
