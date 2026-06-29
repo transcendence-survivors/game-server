bbbf6d37e2e4c6e5731b8ef9dda8d5315e117813
Thyanoui Lutz <tlutz@student.42lyon.fr>
2026-06-29 15:25:45 +0200

    ADD map sync with colyseus server

--- src/GameRoom.ts
+++ src/GameRoom.ts
@@ -6,11 +6,27 @@ import {
 	MAX_DT,
 	MoveInput,
 	MovementState,
+	ACCESS_RADIUS,
+	RAY_DIR_X,
+	RAY_DIR_Z,
+	RAY_SPEED,
+	clampToRadius,
 } from '../../shared-package';
+import { World } from '../../client/src/map/world';
 
 export class GameRoom extends Room {
+	private world!: World;
 	onCreate() {
 		this.state = new GameState();
+		this.world = new World(Math.floor(Math.random() * 1e9));
+		this.setSimulationInterval((dt) => {
+			this.state.rayX += RAY_DIR_X * RAY_SPEED * (dt / 1000);
+			this.state.rayZ += RAY_DIR_Z * RAY_SPEED * (dt / 1000);
+			this.state.rayY = this.world.height(
+				this.state.rayX,
+				this.state.rayZ,
+			);
+		}, 1000 / 20);
 		this.onMessage('move', (client: Client, message: MoveInput) => {
 			const player = this.state.players.get(client.sessionId);
 			if (!player) return;
@@ -29,7 +45,17 @@ export class GameRoom extends Room {
 				z: player.z,
 				rotationY: player.rotationY,
 			};
-			const newState = applyMovement(currentState, clampedInput);
+			let newState = applyMovement(currentState, clampedInput);
+			const { x, z } = clampToRadius(
+				newState.x,
+				newState.z,
+				this.state.rayX,
+				this.state.rayZ,
+				ACCESS_RADIUS,
+			);
+			newState.x = x;
+			newState.z = z;
+			player.y = this.world.height(newState.x, newState.z);
 			const allowed = true;
 			if (allowed) {
 				player.x = newState.x;
@@ -42,6 +68,7 @@ export class GameRoom extends Room {
 
 	onJoin(client: Client) {
 		this.state.players.set(client.sessionId, new Player());
+		client.send('worldSeed', { seed: this.world.seed });
 	}
 
 	onLeave(client: Client) {
